25e6e2a78292d9b2cae14529c7849641066cad09
Dylan Byars <dylanbyars@system76-pc.localdomain>
2021-08-22 00:21:48 -0400

    grouped transactions

--- index.ts
+++ index.ts
@@ -1,33 +1,65 @@
-// import { groupBy } from "https://deno.land/std@0.105.0/collections/mod.ts";
-import { readCSVObjects } from "https://deno.land/x/csv/mod.ts";
+// import * as stringSimilarity from 'https://deno.land/x/string_similarity/mod.ts'
+import {
+  mapValues,
+  groupBy,
+} from 'https://deno.land/std@0.105.0/collections/mod.ts'
+import { readCSVObjects } from 'https://deno.land/x/csv/mod.ts'
 
-const files = Deno.args;
+const COMMON_STORES = [
+  'Amazon',
+  'Paypal',
+  'Wegmans',
+  'Whole Foods',
+  'Harris Teeter',
+]
 
-const totals: Record<string, number> = {};
-
-async function pullData(pathToFile: string) {
-  type Row = {
-    "Transaction Date": string;
-    "Posted Date": string;
-    "Card No.": string;
-    Description: string;
-    Category: string;
-    Debit: string;
-    Credit: string;
-  };
-
-  const file = await Deno.open(pathToFile);
-
-  for await (const row of readCSVObjects(file)) {
-    const { Category, Credit, Debit } = row as Row;
-    const [credit, debit] = [Credit, Debit].map(n => n ? parseFloat(n) : 0);
-    totals[Category] = totals[Category] || 0;
-    totals[Category] = debit - credit + totals[Category];
-  }
-
-  file.close();
+function formatAsCurrency(number: number) {
+  const fixedNumber = Math.abs(number).toFixed(2)
+  return `${number < 0 ? '-' : ''}$${fixedNumber}`
 }
 
-await pullData(files[0]);
+const files = Deno.args
 
-console.log(totals);
+const totalsByCategory: Record<string, number> = {}
+const totalsByDescription: Record<string, number> = {}
+
+const descriptions = new Set()
+
+type Row = {
+  'Transaction Date': string
+  'Posted Date': string
+  'Card No.': string
+  Description: string
+  Category: string
+  Debit: string
+  Credit: string
+}
+
+async function pullData(pathToFile: string) {
+  const file = await Deno.open(pathToFile)
+
+  const content: any = []
+
+  for await (const row of readCSVObjects(file)) {
+    const { Category, Credit, Debit, Description } = row as Row
+    const [credit, debit] = [Credit, Debit].map((n) => (n ? parseFloat(n) : 0))
+    totalsByCategory[Category] =
+      debit - credit + (totalsByCategory[Category] || 0)
+    totalsByDescription[Description] =
+      debit - credit + (totalsByDescription[Description] || 0)
+
+    content.push(row)
+
+    descriptions.add(Description)
+  }
+
+  file.close()
+
+  return content
+}
+
+const c = await pullData(files[0])
+
+const grouped = groupBy<Row>(c, (row) => row.Category)
+console.log(grouped)
+console.log(mapValues(totalsByCategory, formatAsCurrency))
